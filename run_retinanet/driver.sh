81418f27861467b45d95b6d65aaecbc2277d0507
sreenidhia <sreenidhia@graphcore.ai>
2022-04-22 23:36:10 +0100

    use driver script properly

--- run_retinanet/driver.sh
+++ run_retinanet/driver.sh
@@ -1,2 +1,3 @@
-./run_retinanet.sh 2.5.0+896-EA.1
-./run_retinanet.sh 2.5.0+897-EA.1
+CURR_DIR="`dirname $0`"
+$CURR_DIR/run_retinanet.sh 2.5.0+896-EA.1 $1
+$CURR_DIR/run_retinanet.sh 2.5.0+897-EA.1 $1
--- run_retinanet/run_retinanet.sh
+++ run_retinanet/run_retinanet.sh
@@ -1,67 +1,45 @@
 PERSONAL_ACCESS_TOKEN=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
 POPRUN_PREFIX='poprun --num-replicas 4 --ipus-per-replica 4 --num-instances 4'
 
-run_retinanet_sdk() {
-  echo "************************ RUNNING $1 ************************"
-  banner $1
-  
-  SDK_VERSION=$1
-  WORKING_DIR=sdk_$SDK_VERSION
+echo "************************ RUNNING $1 ************************"
+banner $1
 
-  mkdir $WORKING_DIR
-  pushd $WORKING_DIR
+SDK_VERSION=$1
+WORKING_DIR=$2/sdk_$SDK_VERSION
 
-  # clone repo
-  git clone https://$PERSONAL_ACCESS_TOKEN@github.com/graphcore/retinanet_mlperf.git retinanet_mlperf
+mkdir $WORKING_DIR
+pushd $WORKING_DIR
 
-  pushd retinanet_mlperf/retinanet_ipu
-  VIRTUALENV=venv3.6_sdk$SDK_VERSION
-  virtualenv -p /usr/bin/python3.6 $VIRTUALENV
-  source $VIRTUALENV/bin/activate
-  pip install -r requirements.txt
-  popd
+# clone repo
+git clone https://$PERSONAL_ACCESS_TOKEN@github.com/graphcore/retinanet_mlperf.git retinanet_mlperf
 
-  # Download the sdk in current folder
- popsdk-download --dir $PWD --choose $SDK_VERSION
+pushd retinanet_mlperf/retinanet_ipu
+VIRTUALENV=venv3.6_sdk$SDK_VERSION
+virtualenv -p /usr/bin/python3.6 $VIRTUALENV
+source $VIRTUALENV/bin/activate
+pip install -r requirements.txt
+popd
 
-  # untar the sdk tarball
-  for i in *.tar.gz;do
-    tar -xvzf $i
-  done
+# Download the sdk in current folder
+popsdk-download --dir $PWD --choose $SDK_VERSION
 
-  set -- ./poplar*/
-  SDK_FOLDER=$1
+# untar the sdk tarball
+for i in *.tar.gz;do
+  tar -xvzf $i
+done
 
-  pushd $SDK_FOLDER
-  find . -type f -name 'tensorflow-1*intel_skylake512-cp36-cp36m-linux_x86_64.whl' -exec pip install {} \;
-  POPLAR_FOLDER=$(find . -type d -name 'poplar-ubuntu*')
-  source $POPLAR_FOLDER/enable.sh
-  popd
+set -- ./poplar*/
+SDK_FOLDER=$1
 
-  pushd retinanet_mlperf/retinanet_ipu
-  wget https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth
-  make
-  POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "debug.allowOutOfMemory":"true", "autoReport.directory":"./pv"}' $POPRUN_PREFIX python3 train.py --config 'retinanet_default_config' 2>&1 | tee LOG
-  popd
-  popd
-}
+pushd $SDK_FOLDER
+find . -type f -name 'tensorflow-1*intel_skylake512-cp36-cp36m-linux_x86_64.whl' -exec pip install {} \;
+POPLAR_FOLDER=$(find . -type d -name 'poplar-ubuntu*')
+source $POPLAR_FOLDER/enable.sh
+popd
 
-# echo "************************ RUNNING 2.5.0+895-EA.1 ************************"
-# banner 2.5.0+895-EA.1
-# run_retinanet_sdk 2.5.0+895-EA.1
-
-echo "************************ RUNNING 2.5.0+896-EA.1 ************************"
-banner 2.5.0+896-EA.1
-run_retinanet_sdk 2.5.0+896-EA.1
-
-# echo "************************ RUNNING 2.5.0+932 ************************"
-# banner 2.5.0+932
-# run_retinanet_sdk 2.5.0+932
-
-# echo "************************ RUNNING 2.5.0+942 ************************"
-# banner 2.5.0+942
-# run_retinanet_sdk 2.5.0+942
-
-# echo "************************ RUNNING 2.5.0+953 ************************"
-# banner 2.5.0+953
-# run_retinanet_sdk 2.5.0+953
+pushd retinanet_mlperf/retinanet_ipu
+wget https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth
+make
+POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "debug.allowOutOfMemory":"true", "autoReport.directory":"./pv"}' $POPRUN_PREFIX python3 train.py --config 'retinanet_default_config' 2>&1 | tee LOG
+popd
+popd
